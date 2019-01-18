5206a3ecebe867e716a3e9ddb45a6c6a64b24493
Luis Garcia Alanis <luis@garcia.tv>
2019-01-17 20:53:39 -0800

    lint fixes

--- db/config/config.js
+++ db/config/config.js
@@ -1,30 +1,30 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_SCHEMA,
-    "host": "localhost",
-    "dialect": "mysql",
-    "seederStorage": "sequelize",
-    "operatorsAliases": Sequelize.Op,
-  },
-  "test": {
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_SCHEMA,
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "logging": true,
-    "operatorsAliases": Sequelize.Op,
-  },
-  "production": {
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_SCHEMA,
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": Sequelize.Op,
-  }
-}
+    development: {
+        username: 'root',
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA,
+        host: 'localhost',
+        dialect: 'mysql',
+        seederStorage: 'sequelize',
+        operatorsAliases: Sequelize.Op,
+    },
+    test: {
+        username: 'root',
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA,
+        host: '127.0.0.1',
+        dialect: 'mysql',
+        logging: true,
+        operatorsAliases: Sequelize.Op,
+    },
+    production: {
+        username: 'root',
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA,
+        host: '127.0.0.1',
+        dialect: 'mysql',
+        operatorsAliases: Sequelize.Op,
+    },
+};
