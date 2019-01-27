a8c644b096920580d043e6673338f6f9b9abda80
Luis Garcia Alanis <luis@garcia.tv>
2019-01-27 00:47:40 -0600

    custom urls model

--- app/stores/MiniURLDB/Models/index.js
+++ app/stores/MiniURLDB/Models/index.js
@@ -1,11 +1,12 @@
 const Urls = require('./Urls');
-
+const CustomUrls = require('./CustomUrls');
 /**
  * Initializes the SQL models.
  */
 class Models {
     init(sequelize) {
         this.urls = sequelize.import('urls', Urls);
+        this.customUrls = sequelize.import('customUrls', CustomUrls);
         return true;
     }
 }
